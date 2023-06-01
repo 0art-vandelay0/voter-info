c8e429272ebddc3e9298fac7b09277e9d2d73400
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-01 11:21:52 -0700

    Add if/else statement logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ window.onload = function () {
     form.onsubmit = function (event) {
         event.preventDefault();
 
-        let drinkMenu = document.getElementById("voting");
-        drinkMenu.setAttribute("class", "hidden");
-        let under18Message = document.getElementById("under-21");
+        let votingResources = document.getElementById("voting");
+        votingResources.setAttribute("class", "hidden");
+        let under18Message = document.getElementById("under-18");
         under18Message.setAttribute("class", "hidden");
 
         const age = parseInt(document.querySelector("input#age").value);
 
-        if (age > 21) {  // updated to check if age is greater than 21
-            drinkMenu.removeAttribute("class");
-        } else if (age === 21) { // new else if statement
-            drinkMenu.removeAttribute("class");
+        if (age > 18) {
+            votingResources.removeAttribute("class");
+        } else if (age === 18) {
+            votingResources.removeAttribute("class");
         } else {
             under18Message.removeAttribute("class");
         }
